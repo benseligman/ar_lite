cfbfd41aa17cc7362cb6ca3fc150389ceba3aaa5
Ben Seligman <seligman.ben@gmail.com>
2013-08-05 10:17:57 -0700

    complete initial MassObject specs
    - #set_attrs
    - .initialize with mass-assignment

--- lib/active_record_lite/mass_object.rb
+++ lib/active_record_lite/mass_object.rb
@@ -1,19 +1,25 @@
 class MassObject
   def self.set_attrs(*attributes)
     @attributes = attributes
-    attrs.each do |attr|
-      define_method(attr) { attr }
-      define_method("#{attr}=(val)") { attr = val }
-    end
+
+    attributes.each { |attribute|  attr_accessor attribute }
+    nil
   end
 
   def self.attributes
-
+    @attributes
   end
 
   def self.parse_all(results)
   end
 
   def initialize(params = {})
+    params.each do |attr_name, value|
+      unless self.class.attributes.include?(param)
+        raise "mass assignment to unregistered attribute #{attr_name}"
+      end
+
+      self.send("#{attr_name}=", value)
+    end
   end
 end
